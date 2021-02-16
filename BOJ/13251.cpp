58a11fff2d20940e21091cfcf2f4dbcdcb450220
jinho9610 <jinho9610@naver.com>
2021-02-16 16:23:28 +0900

    dfs

--- BOJ/13251.cpp
+++ BOJ/13251.cpp
@@ -14,11 +14,57 @@ using namespace std;
 
 typedef long long ll;
 
+int n, m, k, arr[51], tmp;
+vector<int> stone_set;
+
+void dfs(int idx, int cnt, int among)
+{
+    if (cnt == k)
+    {
+        tmp++;
+        return;
+    }
+
+    for (int i = idx + 1; i <= among; i++)
+        dfs(i, cnt + 1, among);
+}
+
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
+
+    fill(arr, arr + 51, 1);
+
+    cin >> m;
+    for (int i = 0; i < m; i++)
+    {
+        int a;
+        cin >> a;
+        n += a;
+        stone_set.push_back(a);
+    }
+
+    cin >> k;
+
+    ll result = 0;
+    for (int i = 0; i < m; i++)
+    {
+        if (stone_set[i] >= k)
+        {
+            tmp = 0;
+            dfs(0, 0, stone_set[i]);
+            result += tmp;
+        }
+    }
+
+    // 전체 돌 n개 중 k개를 뽑는 경우의 수
+    tmp = 0;
+    dfs(0, 0, n);
+    cout << fixed;
+    cout.precision(15);
+    cout << (double)result / (double)tmp << endl;
 
     return 0;
 }
