4c8f9b331eda83bdbff316fe1414af2df5ed2fa2
JinhoKim <jinho9610@naver.com>
2021-09-02 21:14:45 +0900

    ~21.09.02. 잔디심기

--- prac/prac.cpp
+++ prac/prac.cpp
@@ -1,99 +1,60 @@
-#include <iostream>
-#include <vector>
 #include <string>
-#include <ctype.h>
+#include <vector>
+#include <iostream>
 
 using namespace std;
 
-int arr[5];
-int select[5];
-vector<int> v;
-
-void combi(int idx, int cnt) // ì¡°í•©
+int solution(string s)
 {
-    cout << "idx: " << idx << "    cnt: " << cnt << endl;
-    if (cnt == 3)
+    int answer = 0;
+
+    for (int ul = 1; ul <= s.length(); ++ul) // ´ÜÀ§
     {
-        cout << "cnt == 3: ";
-        for (int i = 0; i < 5; i++)
+        if (s.length() % ul != 0)
+            continue;
+
+        string ans = "";
+        for (int i = 0; i <= s.length(); i += ul)
         {
-            if (select[i])
-                cout << arr[i] << ' ';
+            int a = i, b = i + ul;
+            string token;
+            token = s.substr(a, b);
+            cout << "i: " << a << "\ti + ul: " << b << "\tul: " << ul << "\ttoken: " << token << endl;
+
+            int cnt = 1;
+
+            for (int j = i + ul; j <= s.length(); j += ul)
+            {
+                if (token == s.substr(j, j + ul))
+                {
+                    i = j + ul;
+                    cnt++;
+                }
+
+                else
+                    break;
+            }
+
+            cout << "cnt: " << cnt << endl;
+
+            if (cnt == 1)
+                ans += token;
+            else
+                ans = ans + to_string(cnt) + token;
         }
-        cout << endl;
-        return;
+        cout << "´ÜÀ§ ±æÀÌ: " << ul << " - ¾ÐÃà °á°ú: " << ans << endl;
     }
 
-    for (int i = idx; i < 5; i++)
-    {
-        if (select[i])
-            continue;
-        select[i] = true;
-        combi(i, cnt + 1); // ë°©ë¬¸í•˜ì§€ ì•Šì€ ê³³ì´ë©´ dfs
-        select[i] = false;
-    }
-
-    cout << "others completed:::   idx: " << idx << "     cnt: " << cnt << endl;
-}
-
-void repeat_combi(int idx, int cnt)
-{
-    if (cnt == 3)
-    {
-        for (int i = 0; i < 3; i++)
-            cout << select[i] << ' ';
-        cout << '\n';
-        return;
-    }
-
-    for (int i = idx; i < 5; i++)
-    {
-        select[cnt] = arr[i];
-        repeat_combi(i, cnt + 1);
-    }
-}
-
-void permute(int cnt) // ìˆœì—´
-{
-    if (cnt == 3)
-    {
-        for (auto n : v)
-            cout << n << ' ';
-        cout << '\n';
-        return;
-    }
-
-    for (int i = 0; i < 5; i++)
-    {
-        if (select[i])
-            continue;
-
-        select[i] = true;
-        v.push_back(arr[i]);
-        permute(cnt + 1);
-        v.pop_back();
-        select[i] = false;
-    }
+    return answer;
 }
 
 int main()
 {
-    // ì–´ë–¤ ë°°ì—´ì„ ê°€ì§€ê³  ìˆœì—´ì„ ë§Œë“œëŠ” ë²•
-
-    for (int i = 0; i < 5; i++)
-        arr[i] = i + 1;
-
-    // combi(0, 0);
-    // permute(0);
-    // repeat_combi(0, 0);
-
-    string s = "jinho";
-
-    int a;
+    //freopen("input.txt", "r", stdin);
+    string a;
     cin >> a;
-
-    if (isdigit(a + '0'))
-        cout << "number" << endl;
-
-    return 0;
+    //cout << solution(a) << endl;
+    cout << a.substr(1, 2) << endl;
+    cout << a.substr(2, 3) << endl;
+    cout << a.substr(3, 4) << endl;
 }
